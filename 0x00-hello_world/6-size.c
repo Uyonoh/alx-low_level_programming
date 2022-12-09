6ea03a74e9dd01f4119a0c8db971d0588737a6fb
Uyonoh <turakiuyonoh@gmail.com>
2022-12-09 01:31:50 +0100

    edit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@ int main(void)
 	long long int lli;
 	float f;
 
-	puts("Size of a char: %lu byte(s)", (unsigned long)(sizeof(c)));
-	puts("Size of an int: %lu byte(s)", (unsigned long)(sizeof(c)));
-	puts("Size of a long int: %lu byte(s)", (unsigned long)(sizeof(c)));
-	puts("Size of a long long int: %lu byte(s)", (unsigned long)(sizeof(lli)));
-	puts("Size of a float: %lu byte(S)", (unsigned long)(sizeof(f)));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)(sizeof(c)));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)(sizeof(i)));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)(sizeof(li)));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)(sizeof(lli)));
+	printf("Size of a float: %lu byte(S)\n", (unsigned long)(sizeof(f)));
 
 	return (0);
 }
