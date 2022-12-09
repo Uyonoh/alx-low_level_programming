bc73e6f3085ea4ad7e9198716279d66dbe04fdf6
Uyonoh <turakiuyonoh@gmail.com>
2022-12-09 02:02:45 +0100

    edit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <std.io>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of artis usefull\" - Dora Korpar, 2015-10-19\n");
+	return(1);
+}
