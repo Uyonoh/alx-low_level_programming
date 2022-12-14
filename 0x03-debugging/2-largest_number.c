b8980dbbcecbbf06521426a1560de65743be490e
Uyonoh <turakiuyonoh@gmail.com>
2022-12-14 23:14:04 +0100

    edit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,10 @@ int largest_number(int a, int b, int c)
     largest = a;
 
     if (b > largest)
-    {
         largest = b;
-    }
+
     if (c > largest)
-    {
         largest = c;
-    }
 
     return (largest);
 }
