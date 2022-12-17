10f1c23248e5ca65129d663c0fb2c92a7730d89c
Uyonoh <turakiuyonoh@gmail.com>
2022-12-17 01:32:37 +0100

    edit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <math.h>
 #include "main.h"
 
 /**
@@ -11,33 +13,83 @@ void print_number(int n)
 	get_int(n);
 }
 
-int get_int(int n)
+/**
+  * print_number -  Gets and Prints an integer.
+  * @n: The integer to prints.
+  *
+  * Return: Always 0
+  */
+int get_int( int n)
 {
 	int k = n;
 	int j;
 	int i;
+	int x;
+	int z;
 
-	if (n < 10)
+	if (n < 0)
+	{
+		_putchar('-');
+		k = -n;
+		n = -n;
+	}
+
+	if ((n < 10) && (n >= 0))
 	{
 		_putchar('0' + n);
 		return (0);
 	}
-
+	
+	i = 0;
 	while (k >= 10)
 	{
 		k /= 10;
 		i++;
 	}
 	_putchar('0' + k);
+	
 	if (i == 1)
 	{
-		_putchar('0' + (k % 10));
+		_putchar('0' + (n % 10));
 		return (0);
 	}
-
-	j = k * (10 ^ i);
+	x = pow(10, i);
+	j = k * x;
 	j = n - j;
+
+	z = ilen(n) - ilen(j);
+
+	while (z > 1)
+	{
+		_putchar('0');
+		z--;
+	}
+	
 
 	get_int(j);
 	return (0);
+}
+
+/**
+ * ilen - gets the length of an integer
+ * @n: integer to be checked
+ *
+ * Return: length of integer
+  */
+
+int ilen(int n)
+{
+	int i;
+
+	i = 0;
+
+	if (n == 0)
+		i = 1;
+
+	while (n > 0)
+	{
+		n /= 10;
+		i ++;
+	}
+	return i;
 }
