51aaec8b6d6eeda0c5e4392ab5337278f06ed384
Uyonoh <turakiuyonoh@gmail.com>
2022-12-22 23:39:56 +0100

    edits

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,63 +1,6 @@
 #include "main.h"
 
 /**
- * _strin - checks if a character is in a string
- * @str: string
- * @c: character
- *
- * Return: 1 or 0
- */
-
-int _strin(char *str, char c)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (c == str[i])
-		{
-			return (1);
-		}
-		i++;
-	}
-	return (0);
-}
-
-/**
- * _index - get the index of a character in a string disregaurding case
- * @str: string
- * @c: character
- *
- * Return: index of c
- */
-
-int _index(char *str, char c)
-{
-	int i;
-
-	if (c >= 65 && c <= 90)
-		c += 32;
-
-	if (_strin(str, c) || _strin(str, c - 32))
-	{
-		i = 0;
-		while (str[i])
-		{
-			if (str[i] == c || str[i] == c - 32)
-			{
-				break;
-			}
-			i++;
-		}
-	} else
-	{
-		i = -1;
-	}
-	return (i);
-}
-
-/**
  * leet - encode a string into 1337
  * @str: string
  *
@@ -67,19 +10,23 @@ int _index(char *str, char c)
 char *leet(char *str)
 {
 	int i;
-	int index;
+	int j;
 	char letters[] = "aeotl";
 	int numbers[] = {'4', '3', '0', '7', '1'};
 
 	i = 0;
+
 	while (str[i])
 	{
-		if (_index(letters, str[i]) >= 0)
+		for (j = 0; j <= 4; j++)
 		{
-			index = _index(letters, str[i]);
-			str[i] = numbers[index];
+			if ((str[i] == letters[j]) || (str[i] + 32 == letters[j]))
+			{
+				str[i] = numbers[j];
+			}
 		}
 		i++;
 	}
+
 	return (str);
 }
